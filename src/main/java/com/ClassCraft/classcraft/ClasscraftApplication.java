9bbed7de8387e9d1a36287c1287881e2a0269ff6
mehdi <faddak.mehdi@gmail.com>
2025-04-12 20:34:18 +0100

    initializr

---
+++ src/main/java/com/ClassCraft/classcraft/ClasscraftApplication.java
@@ -0,0 +1,13 @@
+package com.ClassCraft.classcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasscraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasscraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ClassCraft/classcraft/ClasscraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClassCraft.classcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasscraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
